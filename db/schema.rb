c55165078fa70de160f6fb7b394a9a43f7299e79
msimon42 <msimon@uab.edu>
2019-12-02 16:24:18 -0700

    Create database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191120211822) do
+ActiveRecord::Schema.define(version: 20191202232126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 20191120211822) do
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.text "content"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_reviews_on_shelter_id"
+  end
+
   create_table "shelters", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -34,4 +45,5 @@ ActiveRecord::Schema.define(version: 20191120211822) do
     t.string "zip"
   end
 
+  add_foreign_key "reviews", "shelters"
 end
