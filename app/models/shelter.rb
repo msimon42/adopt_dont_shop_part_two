f43cf3399582b14f197d2219a8e2e3ff99b13b8a
msimon42 <msimon@uab.edu>
2019-12-02 16:45:01 -0700

    Add reviews relationship
    Co-Authored-By: agallant121 <agallant1201@gmail.com>

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,6 +1,7 @@
 class Shelter < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   has_many :pets, :dependent => :destroy
+  has_many :reviews 
 
   def find_pets
     pets = Pet.where(shelter_id: self.id)
@@ -8,5 +9,5 @@ class Shelter < ApplicationRecord
 
   def pet_count
     self.find_pets.count
-  end     
+  end
 end
