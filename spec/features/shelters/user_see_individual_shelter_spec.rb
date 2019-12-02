ec45e66da35ba32dcb5969ce45c350179cccd7a9
msimon42 <msimon@uab.edu>
2019-12-02 16:46:14 -0700

    Add tests for review display
    Co-Authored-By: agallant121 <agallant1201@gmail.com>

--- spec/features/shelters/user_see_individual_shelter_spec.rb
+++ spec/features/shelters/user_see_individual_shelter_spec.rb
@@ -20,4 +20,33 @@ RSpec.describe 'shelters index page', type: :feature do
     expect(page).to have_content("Zip: #{shelter_1.zip}")
 
   end
+
+  it 'can see reviews' do
+    shelter_1 = Shelter.create(
+      name: 'Shelter of Dog',
+      address: '123 main street',
+      city: 'Denver',
+      state: 'CO',
+      zip: '80234'
+    )
+
+    review_1 = Review.create(
+      title: 'Really bad',
+      rating: 1,
+      content: 'Literally the worst shelter I have ever seen. You should be ashamed of yourselves.',
+      image: 'test.png',
+      created_at: Time.now,
+      shelter_id: shelter_1.id
+
+    )
+
+    visit "shelters/#{shelter_1.id}"
+
+    expect(page).to have_content(shelter_1.title)
+    expect(page).to have_content(shelter_1.rating)
+    expect(page).to have_content(shelter_1.content)
+    expect(page).to have_content(shelter_1.created_at)
+
+    
+  end
 end
