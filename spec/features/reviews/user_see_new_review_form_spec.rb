b6dda3fd03951ac81c238b6376d989342a1f8691
msimon42 <msimon@uab.edu>
2019-12-02 19:09:11 -0700

    Add test for new review
    Co-Authored-By: agallant121 <agallant1201@gmail.com>

---
+++ spec/features/reviews/user_see_new_review_form_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe 'new review form', type: :feature do
+  it 'can render fields' do
+    shelter_1 = Shelter.create(
+      name: 'Shelter of Dog',
+      address: '123 main street',
+      city: 'Denver',
+      state: 'CO',
+      zip: '80234'
+    )
+
+    visit "/shelters/#{shelter_1.id}/add-review"
+
+    expect(page).to have_field('Title')
+    expect(page).to have_field('Image file')
+    expect(page).to have_field('Comment')
+    expect(page).to have_field('Rating')
+
+    fill_in 'Title', with: 'A Terrible Experience with Terrible People'
+    fill_in 'Image file', with: 'pebbles.png'
+    fill_in 'Comment', with: 'When I entered the shelter, a woman by the name of Maud told me my nose was too big. Very bad form.'
+    fill_in 'Rating', with: '1'
+
+    click_button 'Submit'
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}")
+    expect(page).to have_content('A Terrible Experience with Terrible People')
+  end
+end
