5a01ca257981356c1295c1545a9959839ba6fd72
msimon42 <msimon@uab.edu>
2019-12-02 23:04:35 -0700

    Refactor seed file to include factories
    Co-Authored-By: agallant121 <agallant1201@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,13 @@ Review.destroy_all
 Shelter.destroy_all
 Pet.destroy_all
 
-shelter_1 = Shelter.create(
+shelters = [shelter_1 = Shelter.create(
   name: 'Shelter of Dog',
   address: '123 main street',
   city: 'Denver',
   state: 'CO',
   zip: '80234'
-)
+),
 
 shelter_2 = Shelter.create(
   name: 'Animal Center',
@@ -23,7 +23,7 @@ shelter_2 = Shelter.create(
   city: 'Denver',
   state: 'CO',
   zip: '80444'
-)
+),
 
 shelter_3 = Shelter.create(
   name: 'DogLand',
@@ -31,7 +31,7 @@ shelter_3 = Shelter.create(
   city: 'Denver',
   state: 'CO',
   zip: '80848'
-)
+),
 
 shelter_4 = Shelter.create(
   name: 'Center of Pets',
@@ -39,7 +39,7 @@ shelter_4 = Shelter.create(
   city: 'Denver',
   state: 'CO',
   zip: '80218'
-)
+),
 
 shelter_5 = Shelter.create(
   name: 'Animal Shelter',
@@ -47,179 +47,9 @@ shelter_5 = Shelter.create(
   city: 'Denver',
   state: 'CO',
   zip: '80244'
-)
+)]
 
-review_1 = Review.create(
-  title: 'Really bad',
-  rating: 1,
-  content: 'Literally the worst shelter I have ever seen. You should be ashamed of yourselves.',
-  image: 'https://g.foolcdn.com/image/?url=https%3A%2F%2Fg.foolcdn.com%2Feditorial%2Fimages%2F515803%2Fbusinessman-with-thumb-down.jpg&w=700&op=resize',
-  created_at: Time.now,
-  shelter_id: shelter_1.id
-
-)
-
-review_2 = Review.create(
-  title: 'Really good',
-  rating: 5,
-  content: 'Literally the best shelter I have ever seen. You should not be ashamed of yourselves.',
-  created_at: Time.now,
-  shelter_id: shelter_1.id
-
-)
-
-pet_2 = Pet.create(
-  shelter_id: shelter_1.id,
-  image: 'https://i.redd.it/pfk7qbbti1041.jpg',
-  name: 'Louise',
-  description: 'Bichon Frise',
-  approx_age: 3,
-  sex: 'Female',
-  adoptable?: true
-
-  )
-
-  pet_3 = Pet.create(
-    shelter_id: shelter_2.id,
-    image: 'https://i.redd.it/hfnbnni572041.jpg',
-    name: 'Snowball',
-    description: 'Black Cat',
-    approx_age: 3,
-    sex: 'male',
-    adoptable?: true
-
-    )
-
-    pet_4 = Pet.create(
-      shelter_id: shelter_3.id,
-      image: 'https://i.redd.it/zb81apf641041.jpg',
-      name: 'Kitty',
-      description: 'Grey Cat',
-      approx_age: 4,
-      sex: 'male',
-      adoptable?: true
-
-      )
-
-
-      pet_5 = Pet.create(
-        shelter_id: shelter_4.id,
-        image: 'https://i.redd.it/x8bs5trjk2041.jpg',
-        name: 'Spot',
-        description: 'Golden Retriver',
-        approx_age: 3,
-        sex: 'male',
-        adoptable?: true
-
-        )
-
-
-        pet_6 = Pet.create(
-          shelter_id: shelter_5.id,
-          image: 'https://i.redd.it/am0f8jtop2041.jpg',
-          name: 'Lucius',
-          description: 'Spotted Dog',
-          approx_age: 1,
-          sex: 'male',
-          adoptable?: true
-
-          )
-
-          pet_7 = Pet.create(
-            shelter_id: shelter_1.id,
-            image: 'https://i.imgur.com/iKqs301.jpg',
-            name: 'Quackers',
-            description: 'Duckling',
-            approx_age: 5,
-            sex: 'female',
-            adoptable?: true
-
-            )
-            pet_8 = Pet.create(
-              shelter_id: shelter_2.id,
-              image: 'https://i.redd.it/rg5tbcotf4041.jpg',
-              name: 'Gucci',
-              description: 'Golden Retriver',
-              approx_age: 4,
-              sex: 'female',
-              adoptable?: true
-
-              )
-              pet_9 = Pet.create(
-                shelter_id: shelter_3.id,
-                image: 'https://i.redd.it/znir8eora4041.jpg',
-                name: 'Meatloaf',
-                description: 'Brown Dog',
-                approx_age: 5,
-                sex: 'male',
-                adoptable?: true
-
-                )
-                pet_10 = Pet.create(
-                  shelter_id: shelter_4.id,
-                  image: 'https://i.redd.it/5g33a3qlz1041.jpg',
-                  name: 'Scruffert',
-                  description: 'White Cat',
-                  approx_age: 3,
-                  sex: 'male',
-                  adoptable?: true
-
-                  )
-                  pet_11 = Pet.create(
-                    shelter_id: shelter_5.id,
-                    image: 'https://i.redd.it/tev56zd14zz31.png',
-                    name: 'Koko',
-                    description: 'Golden Retriver',
-                    approx_age: 1,
-                    sex: 'male',
-                    adoptable?: true
-
-                    )
-                    pet_12 = Pet.create(
-                      shelter_id: shelter_1.id,
-                      image: 'https://i.redd.it/xhkr5b0sc3041.jpg',
-                      name: 'Doge',
-                      description: 'Doge',
-                      approx_age: 6,
-                      sex: 'male',
-                      adoptable?: true
-
-                      )
-                      pet_13 = Pet.create(
-                        shelter_id: shelter_2.id,
-                        image: 'https://i.redd.it/fxekew520yz31.jpg',
-                        name: 'Wookie',
-                        description: 'Brown Mix',
-                        approx_age: 2,
-                        sex: 'female',
-                        adoptable?: true
-
-                        )
-                        pet_14 = Pet.create(
-                          shelter_id: shelter_3.id,
-                          image: 'https://i.redd.it/k0x3a78eb2041.jpg',
-                          name: 'Fang',
-                          description: 'A cat',
-                          approx_age: 3,
-                          sex: 'male',
-                          adoptable?: true
-
-                          )
-                          pet_15 = Pet.create(
-                            shelter_id: shelter_4.id,
-                            image: 'https://i.redd.it/fnqpdh1vp4041.jpg',
-                            name: 'Popo',
-                            description: 'Large brown dog',
-                            approx_age: 2,
-                            sex: 'male',
-                            adoptable?: true
-
-                            )
-                            pet_16 = Pet.create(
-                              shelter_id: shelter_5.id,
-                              image: 'https://i.redd.it/t4tnbj2vohx31.jpg',
-                              name: 'Oogie',
-                              description: 'Orangutan',
-                              approx_age: 3,
-                              sex: 'male',
-                              adoptable?: true)
+shelters.each do |shelter|
+  FactoryBot.create_list(:random_pet, 15, shelter: shelter)
+  FactoryBot.create_list(:random_review, 10, shelter: shelter)
+end
