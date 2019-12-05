266c01c2ed34b6259daedf32da6675a1c1e5df2f
Alex Gallant <agallant1201@gmail.com>
2019-12-05 11:48:52 -0700

    Create delete favorites spec
    Co-Authored-By: null <msimon42@users.noreply.github.com>

---
+++ spec/features/favorites/delete_all_favorites_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "When I click the Delete All button", type: :feature do
+  before :each do
+    @shelter_1 = create :random_shelter
+    @pets = create_list(:random_pet, 10, shelter: @shelter_1)
+  end
+
+    it "all the favorites are deleted" do
+
+    visit "/pets/#{@pets[1].id}"
+    expect(page).to have_button('Favorite')
+
+    click_on 'Favorite'
+
+    visit "/favorites"
+
+    expect(page).to have_content(@pets[1].name)
+
+    click_button 'Remove All Favorite Pets'
+
+    expect(page).to_not have_content(@pets[1].name)
+    expect(page).to have_content('You have no favorites :(')
+  end
+end
