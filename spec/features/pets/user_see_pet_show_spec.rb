2d64128a2ee49abe37abdd5330b256d5b0c2164f
msimon42 <msimon@uab.edu>
2019-12-04 19:22:58 -0700

    Add test for favoriting pets
    Co-Authored-By: agallant121 <agallant1201@gmail.com>

--- spec/features/pets/user_see_pet_show_spec.rb
+++ spec/features/pets/user_see_pet_show_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'pet show page', type: :feature do
     @shelter_1 = create :random_shelter
     @pets = create_list(:random_pet, 2, shelter: @shelter_1)
   end
-    
+
   it 'can see pet info' do
 
     visit "/pets/#{@pets[0].id}"
@@ -26,6 +26,14 @@ RSpec.describe 'pet show page', type: :feature do
     expect(page).to have_content(@pets[1].approx_age)
     expect(page).to have_content(@pets[1].sex)
     expect(page).to have_content('Not available for adoption')
+  end
+
+  it 'can favorite pet' do
+    visit "/pets/#{@pets[0].id}"
+
+    click_button 'Add favorite'
+
+
   end
 
 end
