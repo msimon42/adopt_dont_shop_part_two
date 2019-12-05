89c8f7783b0471e4221dbb5c8f78155a8201ad14
msimon42 <msimon@uab.edu>
2019-12-04 22:41:55 -0700

    Add sad paths
    Co-Authored-By: agallant121 <agallant1201@gmail.com>

--- spec/features/favorites/user_see_favorites_spec.rb
+++ spec/features/favorites/user_see_favorites_spec.rb
@@ -6,36 +6,60 @@ RSpec.describe 'When I visit the favorites index page', type: :feature do
     @pets = create_list(:random_pet, 10, shelter: @shelter_1)
   end
 
+  it 'is empty' do
+    visit '/favorites'
+
+    expect(page).to have_content('You have no favorites :(')
+  end
+
+
   it 'can favorite a pet' do
     visit "/pets/#{@pets[1].id}"
+    expect(page).to have_button('Favorite')
 
     click_on 'Favorite'
 
     expect(page).to have_content('Pet added to favorites.')
     expect(page).to have_content('Favorites(1)')
+    expect(page).to have_button('Remove Favorite')
+
 
     visit "/pets/#{@pets[2].id}"
+    expect(page).to have_button('Favorite')
 
     click_on 'Favorite'
 
     expect(page).to have_content('Pet added to favorites.')
     expect(page).to have_content('Favorites(2)')
+    expect(page).to have_button('Remove Favorite')
+
 
     visit "/pets/#{@pets[3].id}"
+    expect(page).to have_button('Favorite')
 
     click_on 'Favorite'
 
     expect(page).to have_content('Pet added to favorites.')
     expect(page).to have_content('Favorites(3)')
+    expect(page).to have_button('Remove Favorite')
+
+
+    click_on 'Remove Favorite'
+
+    expect(page).to have_button('Favorite')
+    expect(page).to have_content('Favorites(2)')
 
     visit "/favorites"
 
     expect(page).to have_content(@pets[1].name)
     expect(page).to have_content(@pets[2].name)
-    expect(page).to have_content(@pets[3].name)
-    expect(page).to have_css("img[src *= #{@pets[1].image}]")
-    expect(page).to have_css("img[src *= #{@pets[2].image}]")
-    expect(page).to have_css("img[src *= #{@pets[3].image}]")
-  end
 
+    within("#favorite-pet-#{@pets[1].id}") do
+      click_on 'Remove Favorite'
+    end
+
+    expect(page).to have_no_content(@pets[1].name)
+    expect(page).to have_content(@pets[2].name)
+
+  end
 end
