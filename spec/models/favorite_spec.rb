fe9d9ae443834b28469b3e5856b3059b54f0096e
msimon42 <msimon@uab.edu>
2019-12-05 19:46:12 -0700

    Correct method name

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe Favorite do
   it 'can calculate total favorites' do
     favorites = Favorite.new(['1', '2', '3'])
 
-    expect(favorites.count).to eq(3)
+    expect(favorites.total_count).to eq(3)
   end
 
   end
