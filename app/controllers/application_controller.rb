18e81c11ff00e49ac17890e2125a1bafdc5c48de
msimon42 <msimon@uab.edu>
2019-12-05 20:25:17 -0700

    Add warning message for cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :favorite_cookie
 
   def favorite_cookie
+    if !session[:favorites]
+      flash[:secondary] = "This site uses cookies in order to provide you with
+      the best possible experience. By continuing, you agree to have cookies
+      stored on your computer."
+    end
     session[:favorites] ||= Array.new
     @favorites ||= Favorite.new(session[:favorites])
     @favorite_count = session[:favorites].length
