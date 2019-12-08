fd525f7f217e338eb6cd79b8a9ed419f5854447c
msimon42 <msimon@uab.edu>
2019-12-07 17:28:03 -0700

    Update schema
    Co-Authored-By: null <agallant121@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191207223139) do
+ActiveRecord::Schema.define(version: 20191208000431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20191207223139) do
     t.string "sex"
     t.string "adoption_status", default: "true"
     t.bigint "shelter_id"
+    t.string "adoptor_name"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
