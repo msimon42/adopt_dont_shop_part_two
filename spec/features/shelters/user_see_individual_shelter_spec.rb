b09fbe314d2c4ddf8d22627dbbf7967348857431
Alex Gallant <agallant1201@gmail.com>
2019-12-10 11:03:33 -0700

    Add tests to check for shelter show information

--- spec/features/shelters/user_see_individual_shelter_spec.rb
+++ spec/features/shelters/user_see_individual_shelter_spec.rb
@@ -22,13 +22,21 @@ RSpec.describe 'shelters show page', type: :feature do
 
   it 'can see reviews' do
 
-    visit "shelters/#{@shelter_1.id}"
+    visit "/shelters/#{@shelter_1.id}"
 
     @reviews.each do |review|
       expect(page).to have_content(review.title)
       expect(page).to have_content(review.rating)
       expect(page).to have_content(review.content)
       #expect(page).to have_content(review.created_at)
+    end
+
+    it "can show number of pets, shelter rating and number of applications" do
+      visit "/shelters"
+
+      expect(page).to have_content("Pet count: #{@shelter_1.pet_count}")
+      expect(page).to have_content("Applications count: #{@shelter_1.applications}")
+      expect(page).to have_content("Average rating: #{@shelter_1.avg_rating}")
     end
   end
 end
