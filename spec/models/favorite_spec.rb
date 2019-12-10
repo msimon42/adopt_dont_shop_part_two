68aec016453f752b8494dbaa0464117bde1c1a49
Alex Gallant <agallant1201@gmail.com>
2019-12-10 15:35:29 -0700

    Add favorite model spec
    Co-Authored-By: null <msimon42@users.noreply.github.com>

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,8 +1,15 @@
 RSpec.describe Favorite do
-  it 'can calculate total favorites' do
-    favorites = Favorite.new(['1', '2', '3'])
 
-    expect(favorites.total_count).to eq(3)
-  end
+    it 'can calculate pets information' do
 
+      favorites = Favorite.new(['1', '2', '3'])
+      pet_1 = Pet.create(name: "Phil", approx_age: 27, sex: "Male", image: "tst.jpg")
+
+
+      expect(favorites.total_count).to eq(3)
+      favorites.add(pet_1)
+      expect(favorites.total_count).to eq(4)
+      favorites.remove(pet_1)
+      expect(favorites.total_count).to eq(3)
+    end
   end
