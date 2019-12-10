4929b590131217c3c68136ad3c8b171440e79cf7
msimon42 <msimon@uab.edu>
2019-12-09 18:52:34 -0700

    Fix migration
    Co-Authored-By: null <agallant121@users.noreply.github.com>

--- db/migrate/20191209222558_change_adoptor_name_to_adopter_id.rb
+++ db/migrate/20191209222558_change_adoptor_name_to_adopter_id.rb
@@ -1,5 +1,5 @@
 class ChangeAdoptorNameToAdopterId < ActiveRecord::Migration[5.1]
   def change
-    rename_column :pets, :adopter_id, :adopter_id
+    rename_column :pets, :adoptor_name, :adopter_id
   end
 end
