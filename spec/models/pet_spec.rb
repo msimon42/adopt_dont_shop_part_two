411784be8511e0d9529dd40d224ab45d2b682c6b
Alex Gallant <agallant1201@gmail.com>
2019-12-10 14:49:41 -0700

    Add pet model spec
    Co-Authored-By: null <msimon42@users.noreply.github.com>

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -9,4 +9,34 @@ describe Pet, type: :model do
     it {should validate_presence_of :sex}
     it {should validate_numericality_of :approx_age}
   end
-end
+
+  describe 'methods' do
+    before :each do
+      PetApplication.destroy_all
+      Application.destroy_all
+      Pet.destroy_all
+      Shelter.destroy_all
+
+      @shelter = create :random_shelter
+      @pets = create_list(:random_pet, 3, shelter: @shelter)
+      @reviews = create_list(:random_review, 2, shelter: @shelter)
+      @application = create :random_application
+      @favorites = Favorite.new([@pets[0].id.to_s])
+      @application.pets << @pets[0]
+    end
+
+
+    it "can find information on pets" do
+
+      pet_1 = Pet.create(name: "Phil", approx_age: 27, sex: "Male", image: "tst.jpg", adopter_id: "#{@application.id}")
+
+      expect(@pets[0].shelter_name).to eq(@shelter.name)
+      expect(Pet.with_applications).to eq([@pets[0]])
+      expect(Pet.app_count).to eq(1)
+      expect(pet_1.adopter_name).to eq(@application.name)
+
+      @pets[0].remove_favorite(@favorites)
+      expect(@favorites.pets).to eq([])
+      end
+    end
+  end
