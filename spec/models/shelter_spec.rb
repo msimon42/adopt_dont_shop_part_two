531a8522899e85e2139d044a976962f164800900
Alex Gallant <agallant1201@gmail.com>
2019-12-10 12:55:17 -0700

    Add shelter model spec tests
    Co-Authored-By: null <msimon42@users.noreply.github.com>

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -21,16 +21,23 @@ describe Shelter, type: :model do
 
       @shelter = create :random_shelter
       @pets = create_list(:random_pet, 3, shelter: @shelter)
-      @reviews = create_list(:random_review, 5, shelter: @shelter)
+      @reviews = create_list(:random_review, 2, shelter: @shelter)
       @application = create :random_application
       @favorites = Favorite.new([@pets[0].id.to_s, @pets[1].id.to_s, @pets[2].id.to_s])
       @application.pets << @pets[0]
     end
 
     it "can find applications and pet count" do
-    expect(@shelter.applications).to eq(1)
-    expect(@shelter.pet_count).to eq(3)
-    # expect(@shelter.avg_rating).to eq(1)
+      Review.destroy_all
+      review_1 = Review.create(shelter_id: @shelter.id, title: "Decent", content: "Good shelter", rating: 3)
+      review_2 = Review.create(shelter_id: @shelter.id, title: "Worst ever", content: "Bad shelter", rating: 1)
+
+      expect(@shelter.applications).to eq(1)
+      expect(@shelter.pet_count).to eq(3)
+      expect(@shelter.avg_rating).to eq(2)
+      end
     end
-  end
 end
+
+# review_1 = Review.create(title: "Decent", content: "Good shelter", rating: 4, image: "" )
+# review_2 = Review.create(title: "Worst ever", content: "Bad shelter", rating: 0, image: "" )
