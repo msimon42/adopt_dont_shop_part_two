1c3b711be591271e418c34f3ceb16631f2d814cb
msimon42 <msimon@uab.edu>
2019-12-11 23:45:58 -0700

    Fix model test for review sort
    Co-Authored-By: null <agallant121@users.noreply.github.com>

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -9,20 +9,31 @@ RSpec.describe Review, type: :model do
       Shelter.destroy_all
 
       @shelter = create :random_shelter
-      @pets = create_list(:random_pet, 3, shelter: @shelter)
-      @reviews = create_list(:random_review, 2, shelter: @shelter)
-      @application = create :random_application
-      @favorites = Favorite.new([@pets[0].id.to_s])
-      @application.pets << @pets[0]
+
+      @review_1 = Review.create(
+        title: 'This place sucks',
+        content: 'They didnt give me free water',
+        rating: 1,
+        created_at: Faker::Time.between(from: DateTime.now - 100, to: DateTime.now),
+        shelter_id: @shelter.id
+      )
+
+      @review_2 = Review.create(
+        title: 'This place rocks',
+        content: 'They gave me free water',
+        rating: 5,
+        created_at: Faker::Time.between(from: DateTime.now - 365, to: DateTime.now - 101),
+        shelter_id: @shelter.id
+      )
     end
 
 
     it 'can sort reviews by rating' do
-      expect(@shelter.reviews.sort_by_rating.select(:rating).pluck.first).to eq(@shelter.reviews.sort_by_rating.select(:rating).pluck.first)
+      expect(@shelter.reviews.sort_by_rating.first).to eq(@review_2)
     end
 
     it 'can sort reviews by date' do
-      expect(@shelter.reviews.sort_by_date.select(:created_at).pluck.first).to eq(@shelter.reviews.sort_by_date.select(:created_at).pluck.first)
+      expect(@shelter.reviews.sort_by_date.first).to eq(@review_1)
     end
   end
 end
